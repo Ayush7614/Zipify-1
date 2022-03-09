a9d0ffb2ed87cecde9a89aa31455f347f6ba0fec
Pulkit Gupta <76155456+pulkit-30@users.noreply.github.com>
2022-03-09 14:25:28 +0530

    intial commit --zipify-v2

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
 const path = require("path");
-const multer = require("multer");
-
-const error = require("./router/error");
-const compress = require("./router/compress");
-const success = require("./router/success");
-const upload = require("./router/upload");
-const unlink = require("./router/unlink");
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")));
@@ -19,11 +11,6 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.use("/upload", upload);
-app.use("/error", error);
-app.use("/compress", compress);
-app.use("/success", success);
-app.use("/unlink", unlink);
 app.listen(process.env.PORT || 80, () => {
   console.log("Running");
 });
